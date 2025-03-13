5ba7ccc6475365cbde2006f6dedb900c89c2c9ad
Sherwin <87555304+owenlim225@users.noreply.github.com>
2025-03-13 09:29:29 +0800

    LESSON 1 VALIDATION

--- index.php
+++ index.php
@@ -36,13 +36,26 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 <form method="POST" action="<?php htmlspecialchars("PHP_SELF"); ?>">
 
 <input type="text" name="name" value="<?php echo $name; ?>"> <br>
+<span class="error"><?php echo $nameErr;?></span> <br>
 
 <input type="text" name="address" value="<?php echo $address; ?>"> <br>
+<span class="error"><?php echo $addressErr;?></span> <br>
 
 <input type="text" name="email" value="<?php echo $email; ?>"> <br>
+<span class="error"><?php echo $emailErr;?></span> <br>
 
 <input type="submit" value="Submit">
 
-
-
 </form>
+
+<hr>
+
+<?php
+    if($name && $address && $email){
+     
+        echo $name."<br>";
+        echo $address."<br>";
+        echo $email."<br>";
+    }
+
+?>
